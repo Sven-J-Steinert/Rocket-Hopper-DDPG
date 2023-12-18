abec00d5111b97d7bdb0a0a50b4c24e38dc89047
Sven-J-Steinert <svensteinert96@gmail.com>
2023-12-18 03:31:55 +0100

    OUNoise & valve PID

--- hopperenv.py
+++ hopperenv.py
@@ -22,21 +22,39 @@ class HopperEnv(Env):
         # Actions we can take: set pressure between 0 and 7 bar
         self.action_space = Box(low=0.0, high=7.0, shape=(1,), dtype=np.float32) # pressure
         # Altitude range
-        self.observation_space = Box(low=np.array([0.,5., -100.]), high=np.array([0.,5., 100.]), shape=(3,)) # [x_target,x,a]
+        self.observation_space = Box(low=np.array([0.,5., -10.]), high=np.array([0.,5., 10.]), shape=(3,)) # [x_target,x,a]
         # Set simulation time
-        self.sim_time = 16 # [s]
-        self.p_set_old = 0 # [bar]
+        self.sim_time = 5 # [s] total sim time
+        self.tn = 1/60    # [s] step sim time
+        self.h = 1/600    # [s] stepsize ode
+        
+        self.p_actual = 0 # [bar]
+        self.counter = 0 # for constant penalty
+        self.x_old = 0 # for constant penalty
         self.v_old = 0
-        self.check = False
 
         # test trajectory: select 3 random hover points and landing in the end
         self.x_target = random.uniform(1, 3)
+        #self.x_target = 1
 
         
         # Set start altitude and velocity
         self.y = np.array([0.,0.] , dtype=np.float32)
         self.state = np.array([self.x_target,0.,0.] , dtype=np.float32)
         self.error_old = self.x_target - self.y[0]
+
+    def reset(self):
+        # Reset all variables
+        self.state = np.array([self.x_target,0,0])
+        self.y = np.array([0.,0.])
+        self.p_actual = 0
+        self.v_old = 0
+        self.error_old = self.x_target - self.y[0]
+        self.x_target = random.uniform(1, 3)
+        #self.x_target = 1
+        self.sim_time = 5 # [s]
+        self.check = False
+        return self.state
 
         
     def step(self, action):
@@ -46,29 +64,44 @@ class HopperEnv(Env):
         
         
         # Apply action
-
-        # action is the pressure
         p_set = action
-        p_actual = dynamic_restriction(p_set,self.p_set_old)
+        self.p_actual = valve_behavior(p_set,self.p_actual)
 
-        y = self.sim_step(self.y,p_actual)
+        y = self.sim_step(self.y,self.p_actual)
         self.y = y[:-1]
 
         self.state = np.array([self.x_target,y[0],y[2]]) # craft [x_target,x,a]
         
         # Reduce sim_time by a step
-        self.sim_time -= 1/60
+        self.sim_time -= self.tn
 
         # reward small error in gauss dist
+
+        error = self.state[1]-self.x_target
+        scale = 1
         sigma = 0.5 # variance - gauss parameter
-        reward = 1/(sigma*np.sqrt(2*np.pi))* np.exp(-0.5*((self.state[1]-self.x_target)/(sigma))**2)
+        reward = scale*(1/(sigma*np.sqrt(2*np.pi))* np.exp(-0.5*((error)/(sigma))**2)) # small error gauss
+
+        scale = 10
+        sigma = 0.5 # variance - gauss parameter
+        
+        #reward = reward * (1 + scale*(1/(sigma*np.sqrt(2*np.pi))* np.exp(-0.5*((y[2])/(sigma))**2))) # small velocity gauss
+        
+        
+        #if p_set == self.p_set_old and abs(y[0] - self.x_old) > 0.001:
+        #    reward -= self.counter * 0.005
+        #    self.counter += 1
+        #else:
+        #    self.counter = 0
+
+        self.x_old = y[0]
         
         # penalize fast change in p_set
         #if abs(self.p_set_old - p_set) > 0.5:
         #    reward += -10 
 
         # update for next loop
-        self.p_set_old = p_actual
+
         
         # Check if shower is done
         if self.sim_time <= 0: 
@@ -76,10 +109,11 @@ class HopperEnv(Env):
         else:
             done = False
         
-        # Apply temperature noise
+        # Apply sensor noise
+        
         #self.state += random.randint(-1,1)
         # Set placeholder for info
-        info = [p_actual,self.y[1]]
+        info = [self.p_actual,self.y[1]]
         
         # Return step information
         return self.state, reward, done, info
@@ -89,25 +123,12 @@ class HopperEnv(Env):
         # Implement viz
         pass
     
-    def reset(self):
-        # Reset all variables
-        self.state = np.array([self.x_target,0,0])
-        self.y = np.array([0,0])
-        self.p_set_old = 0
-        self.v_old = 0
-        self.error_old = self.x_target - self.y[0]
-        self.x_target = random.uniform(1, 3)
-        self.sim_time = 16 # [s]
-        self.check = False
-        return self.state
-    
     def sim_step(self,y,p):
         p = np.squeeze(p) # make sure its one dimensional
-        h = 1/600              # stepsize in seconds
-        t0 = 0                  # initial time in seconds
-        tn = 1/60               # final time in seconds
         
-        time = np.linspace(t0, tn, int((tn-t0)/h)+1)
+        t0 = 0                  # initial time in seconds
+        
+        time = np.linspace(t0, self.tn, int((self.tn-t0)/self.h)+1)
     
         # update Thrust
         global F_T
@@ -115,7 +136,7 @@ class HopperEnv(Env):
         F_T = F_Thrust_fast(p)
         
         for t in time:
-            y = rk4_e(ode, y, h, t)
+            y = rk4_e(ode, y, self.h, t)
             
         x = y[0]
         v = y[1]
@@ -171,41 +192,16 @@ def rk4_e(f, y, h, t):
     return y + h/6 * (f(t, y) + 2 * f(tk_05, yk_025) + 2 * f(tk_05, yk_05) + f(t+h, yk_075))
 
 
-def dynamic_restriction(p_set,p_set_old):
+def valve_behavior(p_set,p_current):
     # make both inputs scalar
     p_set = np.squeeze(p_set)
-    p_set_old = np.squeeze(p_set_old)
+    p_current = np.squeeze(p_current)
+
+    freq=1/60 #[s^-1]
     
-    # main valve opening / closing restriction
-    # 0 % = 0 bar to 100% = 10 bar
+    T=0.2
+
+    # explicit computation of value after freq time
+    p_actual = p_set*(1-np.exp(-freq/T))+p_current*np.exp(-freq/T)
     
-    dp = (p_set-p_set_old) # [bar]
-    sim_step = 1/60 # [s]
-    dp_dt = dp/sim_step # [bar/s]
-    
-    direction = (dp > 0) # True = opening, False = closing
-
-    # opening restrictions
-    # dp/dt = 10 bar / 0.825 s
-    dp_dt_opening = 10 / 0.825
-
-    # closing restrictions
-    # dp/dt = 10 bar / 1.7 s
-    dp_dt_closing = 10 / 1.7
-
-    # if requested change in pressure is to high
-    # return maximum allowed pressure over the next sim step (1/60 s)
-
-    if direction:
-        # opening
-        if dp_dt > dp_dt_opening:
-            # compute max allowed for new p_set
-            p_set = dp_dt_opening * sim_step + p_set_old
-            
-    if not direction:
-        # closing
-        if dp_dt > dp_dt_closing:
-            # compute max allowed for new p_set
-            p_set = dp_dt_closing * sim_step + p_set_old
-
-    return p_set
+    return p_actual
